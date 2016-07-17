f716b51933382f15405a1acd33161cd661af498e
Abhi <abhishekam.swamy@gmail.com>
2016-07-17 20:29:03 +0530

    Adds Delete and Put methods

--- server.js
+++ server.js
@@ -24,6 +24,46 @@ app.post('/todos', function(req, res){
     res.json(body);
 });
 
+app.delete('/todos/:id', function(req, res){
+    var todoId = parseInt(req.params.id);
+    var todoItem = _.findWhere(todos, {id: todoId});
+
+    if(todoItem){
+        todos = _.without(todos, todoItem);
+        return res.json(todoItem);
+    }else{
+        return res.status(404).send();
+    }
+
+});
+
+app.put('/todos/:id', function(req, res){
+    var todoId = parseInt(req.params.id);
+    var todoItem = _.findWhere(todos, {id: todoId});
+    var validAttributes = {};
+
+    if(!todoItem){
+        return res.status(404).send();
+    }
+
+    if(req.body.hasOwnProperty("completed")){
+        if(_.isBoolean(req.body.completed)){
+            validAttributes.completed = req.body.completed;
+        }else{
+            return res.status(400).send();
+        }
+    }
+    if(req.body.hasOwnProperty("description")){
+        if(_.isString(req.body.description) && req.body.description.trim().length >  0){
+            validAttributes.description = req.body.description.trim();
+        }else{
+            return res.status(400).send();
+        }
+    }
+    _.extend(todoItem, validAttributes);
+    res.json(todoItem);
+});
+
 app.get('/todos/:id', function(req, res){
     var todoId = parseInt(req.params.id);
     var todoItem = _.findWhere(todos, {id: todoId});
