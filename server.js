1b0da99183ddca6972775116e9e1673705c3ec89
Abhi <abhishekam.swamy@gmail.com>
2016-07-17 23:42:24 +0530

    Allows search by description and completed status

--- server.js
+++ server.js
@@ -9,7 +9,19 @@ var todoItemId = 1;
 app.use(bodyParser.json());
 
 app.get('/todos', function(req, res){
-    res.json(todos);
+    var queryParams = req.query;
+    var filteredTodos = todos;
+
+    if(queryParams.hasOwnProperty("completed") && queryParams.completed === "true"){
+        filteredTodos = _.where(filteredTodos, {"completed": true});
+    }else if(queryParams.hasOwnProperty("completed") && queryParams.completed === "false"){
+        filteredTodos = _.where(filteredTodos, {"completed": false});
+    }
+    if(queryParams.hasOwnProperty("q") && queryParams.q.trim().length>0){
+        filteredTodos = _.filter(filteredTodos, function(todoItem){
+            return todoItem.description.toLowerCase().indexOf(queryParams.q.toLowerCase());});
+    }
+    res.json(filteredTodos);
 });
 
 app.post('/todos', function(req, res){
