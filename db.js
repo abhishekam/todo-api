dfa27339a366e5f070579efd0d37daa1e788e5c7
Abhi <abhishekam.swamy@gmail.com>
2016-07-26 11:10:42 +0530

    Adding Postgres support for heroku

--- db.js
+++ db.js
@@ -1,9 +1,19 @@
 var Sequelize = require("sequelize");
 var sqlite = require("sqlite3");
-var sequelize = new Sequelize(undefined, undefined, undefined, {
-    'dialect': 'sqlite',
-    'storage': __dirname + '/data/dev-todo-api.sqlite'
-});
+var env = process.env.NODE_ENV || "development";
+
+var sequelize = null;
+if(env === "production"){
+    sequelize = new Sequelize(process.env.DATABASE_URL,{
+        'dialect': 'postgres'
+    });
+}else{
+    sequelize = new Sequelize(undefined, undefined, undefined, {
+        'dialect': 'sqlite',
+        'storage': __dirname + '/data/dev-todo-api.sqlite'
+    });
+}
+
 
 var db = {};
 db.Todo = sequelize.import(__dirname+'/models/todo.js');
